668184b65b2e2ebc9d2e2f047ebad872b60e5a4f
itsDenMat <dennismateo93@gmail.com>
2022-08-25 21:59:40 -0700

    Add listener in server.js

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Setup for data parsing
+// Setup for data parsing JSON data
 // Useful for API calls
 app.use(express.urlencoded({extemded: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+// Listener 
+app.listen(PORT, () => {
+    console.log(`API server is ready in port ${PORT}!`);
+});
