43726c0a906ac53e6bf6844cf597af94d631caf5
itsDenMat <dennismateo93@gmail.com>
2022-08-29 22:18:38 -0700

    Multiple updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Required dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
 // Will initialize express
 const app = express();
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 3001;
 
 // Setup for data parsing JSON data
 // Useful for API calls
-app.use(express.urlencoded({extemded: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-require("./routes/routes")(app);
+require('./routes/routes')(app);
 
 // Listener 
-app.listen(PORT, () => {
-    console.log(`API server is ready in port ${PORT}!`);
-});
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
